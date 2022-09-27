16f450c8473b10ab16e1cbe794ec2d95a6c1c24e
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-09-27 04:33:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
 class cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 
+   
 }
+
+
+class contaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor) {
+           this.saldo -= valor;
+        
+        } else {
+            console.log('valor indisponivel para saque!')
+        }
+    };
+}
+
+
+
 const cliente1 = new cliente();
-const cliente2 = new cliente();
+
+
+
+const contaCorrenteRicardo = new contaCorrente();
+contaCorrenteRicardo.saldo = '0';
+contaCorrenteRicardo.agencia = '1001';
+
+
+
+
+
+
+
 cliente1.nome = 'Ricardo';
 cliente1.cpf = '11122233309';
 cliente1.agencia = '1001';
 cliente1.saldo = '0';
-cliente1.rg = '123456789'
+cliente1.rg = '123456789';
 
- cliente2.nome = 'alice';
- cliente2.cpf = '11122288809';
- cliente2.agencia = '1001';
- cliente2.saldo = '0';
- cliente2.rg ='987654321'
- 
+const cliente2 = new cliente();
+cliente2.nome = 'alice';
+cliente2.cpf = '11122288809';
+cliente2.agencia = '1001';
+cliente2.saldo = '0';
+cliente2.rg = '987654321';
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo ='100'
+console.log(contaCorrenteRicardo.saldo)
+contaCorrenteRicardo.sacar(500)
 
 
 
 
 
-console.log(cliente1,cliente2);
+
+console.log(cliente1, cliente2);
+console.log(contaCorrenteRicardo.saldo);
+
+
+
+
+
+
