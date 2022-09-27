28e1d22f81696f7960ef25cb89a6745d86583d01
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-09-27 04:10:17 -0300

    create bitbanck js

---
+++ index.js
@@ -0,0 +1,28 @@
+class cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+
+}
+const cliente1 = new cliente();
+const cliente2 = new cliente();
+cliente1.nome = 'Ricardo';
+cliente1.cpf = '11122233309';
+cliente1.agencia = '1001';
+cliente1.saldo = '0';
+cliente1.rg = '123456789'
+
+ cliente2.nome = 'alice';
+ cliente2.cpf = '11122288809';
+ cliente2.agencia = '1001';
+ cliente2.saldo = '0';
+ cliente2.rg ='987654321'
+ 
+
+
+
+
+
+console.log(cliente1,cliente2);
