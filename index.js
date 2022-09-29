97771e16125add20abf5a7ab5646152465bc3045
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-09-29 03:27:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ class cliente {
     cpf;
     rg;
 
-   
+
 }
 
 
 class contaCorrente {
     agencia;
-    saldo;
+    #saldo =0;
 
-    sacar(valor){
-        if (this.saldo >= valor) {
-           this.saldo -= valor;
-        
+    sacar(valor) {
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+
         } else {
             console.log('valor indisponivel para saque!')
         }
-    };
+    }
+
+    depositar(valor) {
+        if (valor >= 1) {
+            this.#saldo += valor;
+            console.log(this.#saldo)
+        }
+    }
 }
+
+
 
 
 
@@ -28,7 +37,6 @@ const cliente1 = new cliente();
 
 
 const contaCorrenteRicardo = new contaCorrente();
-contaCorrenteRicardo.saldo = '0';
 contaCorrenteRicardo.agencia = '1001';
 
 
@@ -40,28 +48,30 @@ contaCorrenteRicardo.agencia = '1001';
 cliente1.nome = 'Ricardo';
 cliente1.cpf = '11122233309';
 cliente1.agencia = '1001';
-cliente1.saldo = '0';
+cliente1.saldo = 0;
 cliente1.rg = '123456789';
 
 const cliente2 = new cliente();
 cliente2.nome = 'alice';
 cliente2.cpf = '11122288809';
 cliente2.agencia = '1001';
-cliente2.saldo = '0';
+cliente2.saldo = 0;
 cliente2.rg = '987654321';
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo ='100'
-console.log(contaCorrenteRicardo.saldo)
-contaCorrenteRicardo.sacar(500)
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.sacar(50)
+console.log(contaCorrenteRicardo)
 
 
 
 
 
 
-console.log(cliente1, cliente2);
-console.log(contaCorrenteRicardo.saldo);
+
+
+
+
 
 
 
