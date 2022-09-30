d614bdc63aa9999bca8a4e89d7aa70f93d56814b
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-09-30 03:44:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ class cliente {
 
 class contaCorrente {
     agencia;
-    #saldo =0;
+    _saldo = 0;
 
     sacar(valor) {
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor;
-
-        } else {
-            console.log('valor indisponivel para saque!')
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor
         }
+
+
     }
 
     depositar(valor) {
-        if (valor >= 1) {
-            this.#saldo += valor;
-            console.log(this.#saldo)
+        if (valor < 1) {
+            return
+
+        }else{
+            this._saldo += valor;
         }
+        
     }
 }
 
@@ -48,19 +51,23 @@ contaCorrenteRicardo.agencia = '1001';
 cliente1.nome = 'Ricardo';
 cliente1.cpf = '11122233309';
 cliente1.agencia = '1001';
-cliente1.saldo = 0;
+cliente1._saldo = 0;
 cliente1.rg = '123456789';
 
 const cliente2 = new cliente();
 cliente2.nome = 'alice';
 cliente2.cpf = '11122288809';
 cliente2.agencia = '1001';
-cliente2.saldo = 0;
+cliente2._saldo = 0;
 cliente2.rg = '987654321';
 
 contaCorrenteRicardo.depositar(100)
 contaCorrenteRicardo.depositar(100)
-contaCorrenteRicardo.sacar(50)
+contaCorrenteRicardo.depositar(100)
+
+const valorSacado = contaCorrenteRicardo.sacar(50)
+console.log(valorSacado)
+
 console.log(contaCorrenteRicardo)
 
 
